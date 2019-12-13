2b4020af65d724005db0fb3dc112329824e9e21b
Ubuntu <ubuntu@ip-172-31-16-34.eu-west-1.compute.internal>
2019-12-13 03:41:53 +0000

    Added mlab credentials

--- Section 3/3.4 End/application/__init__.py
+++ Section 3/3.4 End/application/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
+
 
 app = Flask(__name__)
 
 
 from application import routes
+
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
 
--- Section 4/4.3 End/config.py
+++ Section 4/4.3 End/config.py
@@ -3,7 +3,13 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
 
-    MONGODB_SETTINGS = { 'db' : 'UTA_Enrollment' }
+    MONGODB_SETTINGS = { 
+        'db' : 'todo_app',
+        'username':'josephaws',
+        'password':'josephaws1234',
+        'host': 'ds047792.mlab.com',
+        'port': 47792
+    }
 
 
     
